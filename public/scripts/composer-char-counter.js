8c95c017cf6645dccf68e362056c6484f0e80b85
Todd Demone <tdemone1@gmail.com>
2021-12-08 22:06:04 +0000

    prefer jquery - change  to

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,8 @@ $(document).ready(() => {
   $('#tweet-text').on('input', function(_e) {
     // jQuery assigns the target element (textarea) to `this`,
     // but only if we use ES5 function syntax (can't use fat arrow syntax).
-    // `this.value` is the string entered into the textarea e.g., 'some text'
-    const stringLength = this.value.length; // e.g. => 9
+    // `$(this).val()` is the string entered into the textarea e.g., 'some text'
+    const stringLength = $(this).val().length; // e.g. => 9
     // Grab the counter HTML element
     const counter = $(this).next().children(".counter");
     // Turn the counter red if count reaches 0 or negative
