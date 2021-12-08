75263fdbea351c72baa4463d4ca43d011d99cf53
Todd Demone <tdemone1@gmail.com>
2021-12-08 22:00:36 +0000

    add jQuery to get value inputed into textarea; use value to change text output of counter HTML element

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+
+$(document).ready(() => {
+  $('#tweet-text').on('input', function(_e) {
+    // jQuery assigns the target element (textarea) to `this`,
+    // but only if we use ES5 function syntax (can't use fat arrow syntax).
+    // `this.value` is the string entered into the textarea e.g., 'some text'
+    const stringLength = this.value.length; // e.g. => 9
+    // Grab the counter HTML element
+    const counter = $(this).next().children(".counter");
+    // Turn the counter red if count reaches 0 or negative
+    140 - stringLength < 0 ? counter.addClass('negative') : counter.removeClass('negative');
+    counter.text(140 - stringLength);
+  })
+});
