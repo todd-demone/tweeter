2d305c59f867cc32620836851ff6dae623cdd23c
Todd Demone <tdemone1@gmail.com>
2021-12-10 11:34:04 +0000

    change variable name from stringLength to tweetLength

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,11 @@ $(document).ready(() => {
     // jQuery assigns the target element (textarea) to `this`,
     // but only if we use ES5 function syntax (can't use fat arrow syntax).
     // `$(this).val()` is the string entered into the textarea e.g., 'some text'
-    const stringLength = $(this).val().length; // e.g. => 9
+    const tweetLength = $(this).val().length; // e.g. => 9
     // Grab the counter HTML element
     const counter = $(this).next().children(".counter");
     // Turn the counter red if count reaches 0 or negative
-    140 - stringLength < 0 ? counter.addClass('negative') : counter.removeClass('negative');
-    counter.text(140 - stringLength);
+    140 - tweetLength < 0 ? counter.addClass('negative') : counter.removeClass('negative');
+    counter.text(140 - tweetLength);
   })
 });
