b786b7d69c55511be18c82f220482a5b5f11127f
Todd Demone <tdemone1@gmail.com>
2021-12-11 14:10:50 +0000

    add renderTweets()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,16 +5,37 @@
  */
 
 $(document).ready( () => {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    const $tweetsContainer = $('#tweets-container');
+    tweets.forEach( function(tweet) {
+      const $tweetElement = createTweetElement(tweet);
+      $tweetsContainer.append($tweetElement);
+    })
   };
   
   const createTweetElement = function(tweet) {
@@ -43,6 +64,5 @@ $(document).ready( () => {
     return $tweet;
   }
   
-  const $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
