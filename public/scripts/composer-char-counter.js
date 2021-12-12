9bf6eabf274c2af5bb3916eb0ee37e739363fa58
Todd Demone <tdemone1@gmail.com>
2021-12-12 16:01:51 +0000

    switch to fat arrow function and replace  with ; improve variable names

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
 const MAX_LENGTH = 140;
 $(document).ready(() => {
-  $('#tweet-text').on('input', function(_e) {
+  $('#tweet-text').on('input', (e) => {
     // jQuery assigns the target element (textarea) to `this`,
     // but only if we use ES5 function syntax (can't use fat arrow syntax).
     // `$(this).val()` is the string entered into the textarea e.g., 'some text'
-    const tweetLength = $(this).val().length; // e.g. => 9
-    // Grab the counter HTML element
-    const $counter = $(this).parent().find(".counter");
-    // Turn the counter red if count reaches 0 or negative
+    const $textareaElement = $(e.currentTarget);
+    const $counterElement = $textareaElement.parent().find(".counter");
+    const tweetLength = $textareaElement.val().length;
     const charactersRemaining = MAX_LENGTH - tweetLength;
-    charactersRemaining < 0 ? $counter.addClass('negative') : $counter.removeClass('negative');
-    $counter.text(charactersRemaining);
+
+    charactersRemaining < 0 ? $counterElement.addClass('negative') : $counterElement.removeClass('negative');
+    
+    $counterElement.text(charactersRemaining);
   })
 });
