1f3b084ed9ada77d922a241e2db46994e80dd390
Todd Demone <tdemone1@gmail.com>
2021-12-13 20:53:45 +0000

    add handlers for two events: (1) scroll, and (2) click on 'scroll-to-top' button

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,5 +1,41 @@
 $(document).ready( () => {
+
   $('.navbar button.cta').on('click', e => {
     $('.new-tweet').slideToggle();
+    document.getElementById('tweet-text').focus();
   });
+
+  $(window).on('unload', () => {
+    $(window).scrollTop(0);
+  });
+
+
+  let lastScrollTop = 0;
+
+  $(window).on('scroll', e => {
+    const st = $(window).scrollTop();
+    if (st > lastScrollTop) {
+      $('.navbar button.scroll-to-top').show();
+      $('.navbar button.cta').hide();
+    } else {
+      $('.navbar button.scroll-to-top').hide();
+      $('.navbar button.cta').show();
+    }
+    lastScrollTop = st;
+  })
+
+  $('.navbar button.scroll-to-top').on('click', () => {
+    $('html, body').animate(
+      { scrollTop: 0 }, 
+      () => {
+          $('.navbar button.cta').show();
+          $('.navbar button.scroll-to-top').hide();
+          $('.new-tweet').slideDown();
+          document.getElementById('tweet-text').focus();
+
+      } 
+    );
+    return false;
+  });
+
 });
