b67e0c6cf16b76e735e5e0ef10ed989db1016f00
Todd Demone <tdemone1@gmail.com>
2021-12-17 20:49:37 +0000

    add definition for  to make code cleaner; rewrite event handler description

--- public/scripts/character-counter.js
+++ public/scripts/character-counter.js
@@ -2,18 +2,24 @@ const MAX_LENGTH = 140;
 
 $(() => {
   
-  // When the textarea contents are changed by the user, this event method updates the new tweet box's character counter with the # of allowable characters remaining.
-  // When the new textarea contains > 140 characters, change the character counter's colour to red and show the error message.
-  $('#tweet-text').on('input', (e) => {
+  /**
+   * When the textarea contents are changed by the user, the method
+   * updates the tweet form's character counter with the # of allowable
+   * characters remaining.
+   * When the new textarea contains > 140 characters, the method changes
+   * the character counter's colour to red and show the error message.
+   */
+  $('#tweet-text').on('input', e => {
     const $tweetText = $(e.currentTarget);
     const $counter = $tweetText.parent().find(".counter");
     const tweetLength = $tweetText.val().length;
     const charactersRemaining = MAX_LENGTH - tweetLength;
     const $errorBox = $tweetText.parent().siblings('.error');
+    const $errorMessage = $errorBox.children('.msg');
 
     if (charactersRemaining < 0) {
       $counter.addClass('negative');
-      $errorBox.children('.msg').html("Tweets can't exceed 140 characters.");
+      $errorMessage.html("Tweets can't exceed 140 characters.");
       $errorBox.slideDown();
     } else {
       $counter.removeClass('negative');
