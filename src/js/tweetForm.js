e73626ecf88624d800fb612f3c88ea4dd4ccaa1b
Todd Demone <tdemone1@gmail.com>
2021-12-22 21:38:50 +0000

    combine event handlers into one function - tweetFormEvents()

--- src/js/tweetForm.js
+++ src/js/tweetForm.js
@@ -1,68 +1,55 @@
-// newTweetForm.js
+// tweetForm.js
 
+import { isValidTweet, MAX_LENGTH, sendErrorMessage } from './helpers.js';
 import loadTweets from './tweetsContainer.js';
-import sendErrorMessage from "./helpers.js";
-
-// event: submit form data via 'Tweet' button
-// handler: see postForm() above
-const postDataOnSubmit = () => {
-  $('#tweet-form').on('submit', postFormData);
-};
 
 
-// event: submit form data via Enter key
-// handler: see postForm() above
-const postDataOnEnterKey = () => {
-  $('#tweet-form').on('keydown', e => {
+const tweetFormEvents = () => {
+  const $tweetForm = $('#tweet-form');
+  
+  $tweetForm.on('submit', postFormData);
+  
+  $tweetForm.on('keydown', e => { 
     if (e.keyCode === 13) postFormData(e);
   });
-};
+  
+  $tweetForm.on('input', '#tweet-textarea', e => {
+    const $tweetTextarea = $(e.currentTarget);
+    const tweetString = $tweetTextarea.val();
+    const $counter = $tweetForm.find(".counter");
+    const $errorBox = $tweetForm.siblings('.error');
 
-
-// event: textarea contents are changed by user
-// handler: update counter; if below 0, change to red and show error message;
-const countCharacters = () => {
-  const MAX_LENGTH = 140;
-  $('#tweet-text').on('input', e => {
-    const $tweetText = $(e.currentTarget);
-    const $counter = $tweetText.parent().find(".counter");
-    const tweetLength = $tweetText.val().length;
-    const charactersRemaining = MAX_LENGTH - tweetLength;
-    const $errorBox = $tweetText.parent().siblings('.error');
-    const $errorMessage = $errorBox.children('.msg');
-
-    if (charactersRemaining < 0) {
+    if (tweetString.length > MAX_LENGTH) {
       $counter.addClass('negative');
-      $errorMessage.html("Tweets can't exceed 140 characters.");
-      $errorBox.slideDown();
+      sendErrorMessage(`Tweets can't exceed ${MAX_LENGTH} characters.`);
     } else {
       $counter.removeClass('negative');
       $errorBox.slideUp();
     }
     
-    $counter.text(charactersRemaining);
+    $counter.text(MAX_LENGTH - tweetString.length);
   });
-};
+}
 
-// This function is called when a form is submitted (see above).
-// Validates text in form, POSTs data to server, loads tweets.
+
 const postFormData = e => {
   const $tweetForm = $(e.currentTarget);
-  const tweetString = $tweetForm.children('#tweet-text').val();
   const data = $tweetForm.serialize();
+  const tweetString = $tweetForm.children('#tweet-textarea').val();
   
   e.preventDefault();
 
-  if (!tweetString) return sendErrorMessage("You can't send an empty tweet.");
-  if (tweetString.length > 140) return sendErrorMessage("Tweets can't exceed 140 characters.");
+  if (!isValidTweet(tweetString)) return false;
 
   $.post('/tweets', data)
     .then(() => {
       loadTweets();
-      $tweetForm.trigger('reset');
-      $tweetForm.find(".counter").text('140');
+      $tweetForm
+        .trigger('reset')
+        .find(".counter").text(MAX_LENGTH);
     })
     .catch(err => sendErrorMessage(`Error: Your tweet did not get sent (${err.status} ${err.statusText})`));
 };
 
-export { postDataOnSubmit, postDataOnEnterKey, countCharacters };
+
+export default tweetFormEvents;
