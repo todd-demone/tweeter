f4f7b51151102e41cba58686d7416de5ad48e64e
Todd Demone <tdemone1@gmail.com>
2021-12-22 21:40:26 +0000

    add escapeText(), isValidTweet() and MAX_LENGTH

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,14 +1,37 @@
 // helpers.js
 
+const MAX_LENGTH = 140;
+
+
+// receives string and returns escaped string (prevents XSS)
+const escapeText = str => {
+  let div = document.createElement('div');
+
+  div.appendChild(document.createTextNode(str));
+  
+  return div.innerHTML;
+};
+
+
+const isValidTweet = tweetString => {
+  if (!tweetString) {
+    sendErrorMessage("You can't send an empty tweet.");
+    return false;
+  } else if (tweetString.length > MAX_LENGTH) {
+    sendErrorMessage(`Tweets can't exceed ${MAX_LENGTH} characters.`);
+    return false;
+  } 
+  return true;
+};
+
+
 // receives an error message (string) and renders it to the screen; returns false.
 const sendErrorMessage = message => {
   const $errorBox = $('.new-tweet .error');
-  const $errorMessage = $errorBox.children('.msg');
   
-  $errorMessage.html(message);
+  $errorBox.children('.msg').html(message);
   $errorBox.slideDown();
-  $('#tweet-text').focus();
-  return false;
+  $('#tweet-textarea').focus();
 };
 
-export default sendErrorMessage;
+export { escapeText, isValidTweet, MAX_LENGTH, sendErrorMessage };
