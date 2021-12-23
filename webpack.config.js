c54cb237dea5c50567ba46e9e25d9deb3dc652e9
Todd Demone <tdemone1@gmail.com>
2021-12-23 17:39:35 +0000

    add babel module rules; add watch property

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   mode: "production",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
