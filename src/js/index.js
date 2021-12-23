ff1b0c3e4ed96ae330e7298e6480e86802c55217
Todd Demone <tdemone1@gmail.com>
2021-12-23 18:14:00 +0000

    move back to using arrow function since new babel config is now doing what I want it to do - i.e., convert it to old syntax for older browsers

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import loadTweets from "./tweetsContainer.js";
 import { navButtonsEvents, showHideButtonsOnScroll } from "./navigation.js";
 import tweetFormEvents from "./tweetForm.js";
 
-$(document).ready(function () {
+$(() => {
   loadTweets();
   navButtonsEvents();
   showHideButtonsOnScroll();
