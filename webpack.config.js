7d0079dce61d383747a8db968b1d335f94cec871
Todd Demone <tdemone1@gmail.com>
2021-12-23 18:12:22 +0000

    remove watch and target properties

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,11 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/js/index.js",
-  target: ["web", "es5"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "production",
-  watch: true,
   module: {
     rules: [
       {
