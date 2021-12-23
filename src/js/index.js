337b0843a1308ead2c236351b311d362ed525199
Todd Demone <tdemone1@gmail.com>
2021-12-23 17:40:43 +0000

    replace arrow syntax with class jQuery .ready(function ()...

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 //  index.js
 
-import loadTweets from './tweetsContainer.js';
-import { navButtonsEvents, showHideButtonsOnScroll } from './navigation.js';
-import tweetFormEvents from './tweetForm.js';
+import loadTweets from "./tweetsContainer.js";
+import { navButtonsEvents, showHideButtonsOnScroll } from "./navigation.js";
+import tweetFormEvents from "./tweetForm.js";
 
-$(() => {
+$(document).ready(function () {
   loadTweets();
   navButtonsEvents();
   showHideButtonsOnScroll();
