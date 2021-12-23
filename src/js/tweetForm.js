dec18fe8917ef5cb7226c84e65fb1613f56cfd9a
Todd Demone <tdemone1@gmail.com>
2021-12-23 18:35:20 +0000

    prettier format

--- src/js/tweetForm.js
+++ src/js/tweetForm.js
@@ -1,52 +1,50 @@
 // tweetForm.js
 
-import { isValidTweet, MAX_LENGTH, sendErrorMessage } from './helpers.js';
-import loadTweets from './tweetsContainer.js';
-
+import { isValidTweet, MAX_LENGTH, sendErrorMessage } from "./helpers.js";
+import loadTweets from "./tweetsContainer.js";
 
 const tweetFormEvents = () => {
-  const $tweetForm = $('#tweet-form');
-  
-  // send Tweet to server when user clicks 'Tweet' button
-  $tweetForm.on('submit keydown', e => {
-    if (e.type === 'submit' || e.keyCode === 13) {
+  const $tweetForm = $("#tweet-form");
+
+  // send Tweet to server when user clicks 'Tweet' button or presses `Enter` key
+  $tweetForm.on("submit keydown", (e) => {
+    if (e.type === "submit" || e.keyCode === 13) {
       const data = $tweetForm.serialize();
-      const tweetString = $tweetForm.children('#tweet-textarea').val();
-      
+      const tweetString = $tweetForm.children("#tweet-textarea").val();
+
       e.preventDefault();
-    
+
       if (!isValidTweet(tweetString)) return false;
-    
-      $.post('/tweets', data)
+
+      $.post("/tweets", data)
         .then(() => {
           loadTweets();
-          $tweetForm
-            .trigger('reset')
-            .find(".counter").text(MAX_LENGTH);
+          $tweetForm.trigger("reset").find(".counter").text(MAX_LENGTH);
         })
-        .catch(err => sendErrorMessage(`Error: Your tweet did not get sent (${err.status} ${err.statusText})`));
+        .catch((err) =>
+          sendErrorMessage(
+            `Error: Your tweet did not get sent (${err.status} ${err.statusText})`
+          )
+        );
     }
   });
 
-  
   // update character counter to show how many characters until hit MAX_LENGTH; turn counter red if exceed MAX_LENGTH
-  $tweetForm.on('input', '#tweet-textarea', e => {
+  $tweetForm.on("input", "#tweet-textarea", (e) => {
     const tweetString = $(e.currentTarget).val();
     const $counter = $tweetForm.find(".counter");
-    const $errorBox = $tweetForm.siblings('.error');
+    const $errorBox = $tweetForm.siblings(".error");
 
     if (tweetString.length > MAX_LENGTH) {
-      $counter.addClass('negative');
+      $counter.addClass("negative");
       sendErrorMessage(`Tweets can't exceed ${MAX_LENGTH} characters.`);
     } else {
-      $counter.removeClass('negative');
+      $counter.removeClass("negative");
       $errorBox.slideUp();
     }
-    
+
     $counter.text(MAX_LENGTH - tweetString.length);
   });
-  
 };
-
 
 export default tweetFormEvents;
